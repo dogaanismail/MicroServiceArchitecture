b36592a5fb9ac078ddf01c8ecec7f9ed1b29e621
dogaanismail <ismaildogaan@gmail.com>
2021-11-19 23:28:38 +0300

    Micro service architecture - Course controller has been added into the system.

---
+++ Services/Catalog/Catalog.API/Controllers/CoursesController.cs
@@ -0,0 +1,79 @@
+﻿using Catalog.API.Dtos;
+using Catalog.API.Services;
+using MicroServiceArchitecture.Shared.ControllerBases;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CoursesController : CustomBaseController
+    {
+        #region Fields
+        private readonly ICourseService _courseService;
+
+        #endregion
+
+        #region Ctor
+
+        internal CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        #endregion
+
+        #region Methods
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var response = await _courseService.GetAllAsync();
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(string id)
+        {
+            var response = await _courseService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [Route("/api/[controller]/GetAllByUserId/{userId}")]
+        public async Task<IActionResult> GetAllByUserId(string userId)
+        {
+            var response = await _courseService.GetAllByUserId(userId);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(CourseCreateDto courseCreateDto)
+        {
+            var response = await _courseService.CreateAsync(courseCreateDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(CourseUpdateDto courseUpdateDto)
+        {
+            var response = await _courseService.UpdateAsync(courseUpdateDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(string id)
+        {
+            var response = await _courseService.DeleteAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+        #endregion
+    }
+}
---
+++ Shared/MicroServiceArchitecture.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,16 @@
+﻿using MicroServiceArchitecture.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroServiceArchitecture.Shared.ControllerBases
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
